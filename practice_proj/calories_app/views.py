4f2a7028f86cd2312ff52c78f495efc7aa6af19a
Josef Donocik <joe.donocik@gmail.com>
2022-06-17 11:44:42 +0200

    added route to the calories app

--- practice_proj/calories_app/views.py
+++ practice_proj/calories_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(req):
+    """ Renders (/sends) home page of the calories app """
+    return HttpResponse('<h1>calories app home</h1>')
