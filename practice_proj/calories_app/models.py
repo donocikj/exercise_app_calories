cea0aa667c5e458bef4ba58742abc027b0e75ccb
Josef Donocik <joe.donocik@gmail.com>
2022-06-17 15:02:55 +0200

    added the meal model to database

--- practice_proj/calories_app/models.py
+++ practice_proj/calories_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Meal(models.Model):
+    """ representation of a meal registered to certain date and with a given amount of calories """
+    name = models.CharField(max_length=100)
+    calories = models.FloatField()
+    date = models.DateField(default=timezone.now)
--- practice_proj/practice_proj/settings.py
+++ practice_proj/practice_proj/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'calories_app.apps.CaloriesAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
